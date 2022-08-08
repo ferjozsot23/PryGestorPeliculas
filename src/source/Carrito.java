071df2a34b5ac385ad1519ea00c7945f6209db9d
matucho01 <cristopher.perez01@epn.edu.ec>
2022-08-08 11:32:57 -0500

    nuevo package

--- src/source/Carrito.java
+++ src/source/Carrito.java
@@ -1,3 +1,5 @@
+package source;
+
 public class Carrito {
     public void calcularTotal() {
         System.out.println("Para saber el total ingresa el número de tu tarjeta de crédito y los 3 numeritos de atrás");
