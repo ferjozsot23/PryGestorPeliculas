86083a33d3bc5ac056165995dc865e7145e123b6
Mateo <promateo2001@gmail.com>
2022-09-08 10:50:20 -0500

    Clase Gestor Pelicula

--- src/source/gestorPelicula/GestorPelicula.java
+++ src/source/gestorPelicula/GestorPelicula.java
@@ -4,25 +4,30 @@ import java.util.ArrayList;
 
 public class GestorPelicula {
 
+    Pelicula pelicula = null;
+
+    public GestorPelicula(Pelicula pelicula){
+        this.pelicula = pelicula;
+    }
+
     public String buscarPelicula(String titulo){
         return "";
     }
 
-    public String extraerDatosPelicula(String idPelicula){
-        return "";
+    public ArrayList<String> extraerDatosPelicula(){
+        return pelicula.getInformacion();
     }
 
     public void eliminarPelicula(String idPelicula){
 
     }
 
-    public Pelicula agregarPelicula(){
-        Pelicula peliculaNueva = new Pelicula();
-        return peliculaNueva;
+    public void agregarPelicula(){
+        this.pelicula = new Pelicula();
     }
 
-    public void actualizarPelicula(String idPelicula, ArrayList<String> datosPelicula){
-
+    public void actualizarPelicula(String genero, String elenco, String idioma){
+        pelicula.setInformacion(genero,elenco,idioma);
     }
 
 }
